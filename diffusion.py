809fca5e2fbf9ca1321e78530d49ee456b8c7a95
Tamir Puzanov <58332679+TamirPuzanov@users.noreply.github.com>
2023-08-02 00:35:39 +0300

    Fix diffusion.py

--- diffusion.py
+++ diffusion.py
@@ -1,108 +1,128 @@
+import torch.nn.functional as F
+
 import torch.nn as nn
 import torch
 
-import torch.nn.functional as F
 
-
-def gather(vals, t, x_shape):
-    out = vals.gather(-1, t.cpu())
-    out = out.reshape(t.shape[0], *((1,) * (len(x_shape) - 1)))
-    return out.to(t.device)
-
-
-class Diffusion(nn.Module):
-    def __init__(self, model: nn.Module, criterion=nn.MSELoss(reduction="mean"),
-                       n_steps: int = 1000, betas: torch.Tensor = None):
+class Diffusion:
+    def __init__(self, model:   nn.Module,
+                       n_steps: int          = 1000,
+                       betas:   torch.Tensor = None,
+                       criterion             = F.mse_loss) -> None:
         
-        super().__init__()
-        
-        if betas is None:
-            betas = torch.linspace(0.0001, 0.04, n_steps)
-        
-        assert isinstance(betas, torch.Tensor)
         assert len(betas.shape) == 1
         assert betas.shape[0]   == n_steps
-        assert n_steps          >= 1
-        
-        self.model     = model
-        self.criterion = criterion
-        
+
+        self.model   = model
         self.n_steps = n_steps
-        self.betas   = betas
-        self.calc()
-        
-    def forward(self, x0: torch.Tensor):
+
+        self.betas  = betas
+        self.alphas = 1 - betas
+
+        self.criterion = criterion
+
+        if self.betas is None:
+            self.betas = torch.linspace(0.0001, 0.04, (n_steps,))
+    
+    def forward(self, x0):
         self.model.train()
-        t = torch.randint(
-            0, self.n_steps, (x0.shape[0],), 
-            dtype=torch.long, device=x0.device
-        )
-        xn, eps  = self.forward_diffusion_sample(x0, t, x0.device)
-        eps_pred = self.model(x=xn, t=t)
-        loss = self.criterion(eps_pred, eps)
+
+        t = torch.randint(0, self.n_steps, (x0.shape[0],), device=x0.device)
+
+        x      = self.forward_diffusion_t_batch(x, t)
+        x, eps = self.forward_diffusion_batch(x, t)
+
+        out = self.model(x)
+
+        loss = self.criterion(out, eps)
+
         return loss
+
+    def forward_diffusion_batch(self, x: torch.Tensor, t: torch.Tensor):
+        r, eps = [], []
+
+        for i in range(x.shape[0]):
+            r_, eps_ = self.forward_diffusion(x[i], t[i])
+            r.append(r_[None]); eps.append(eps_[None])
+
+        r   = torch.cat(r,   dim=0)
+        eps = torch.cat(eps, dim=0)
+
+        return r, eps
     
-    def calc(self):
-        self.alphas = 1. - self.betas
-        self.alphas_cumprod = torch.cumprod(self.alphas, axis=0)
-        self.alphas_cumprod_prev = F.pad(self.alphas_cumprod[:-1], (1, 0), value=1.0)
-        self.sqrt_recip_alphas = torch.sqrt(1.0 / self.alphas)
-        self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod)
-        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1.0 - self.alphas_cumprod)
-        self.posterior_variance = self.betas * (1.0 - self.alphas_cumprod_prev) / (1.0 - self.alphas_cumprod)
+    def forward_diffusion_t_batch(self, x: torch.Tensor, t: torch.Tensor):
+        r = []
+
+        for i in range(x.shape[0]):
+            r.append(self.forward_diffusion_t(x[i], t[i])[None])
         
-    def forward_diffusion_sample(self, x_0: torch.Tensor, t: torch.Tensor, device="cpu"):
-        noise = torch.randn_like(x_0)
-        sqrt_alphas_cumprod_t = gather(self.sqrt_alphas_cumprod, t, x_0.shape)
-        sqrt_one_minus_alphas_cumprod_t = gather(
-            self.sqrt_one_minus_alphas_cumprod, t, x_0.shape
-        )
-        return sqrt_alphas_cumprod_t.to(device) * x_0.to(device) \
-        + sqrt_one_minus_alphas_cumprod_t.to(device) * noise.to(device), noise.to(device)
-    
-    def sample_timestep(self, x: torch.Tensor, eps: torch.Tensor, t: torch.Tensor):
-        betas_t = gather(self.betas, t, x.shape)
-        sqrt_one_minus_alphas_cumprod_t = gather(
-            self.sqrt_one_minus_alphas_cumprod, t, x.shape
-        )
-        sqrt_recip_alphas_t = gather(self.sqrt_recip_alphas, t, x.shape)
-        
-        model_mean = sqrt_recip_alphas_t * (
-            x - betas_t * eps / sqrt_one_minus_alphas_cumprod_t
-        )
-        posterior_variance_t = gather(self.posterior_variance, t, x.shape)
-        
+        r = torch.cat(r, dim=0)
+
+        return r
+
+    def forward_diffusion(self, x: torch.Tensor, t: int):
+        eps = torch.randn_like(x)
+        alpha = self.alphas[t]
+
+        return x * (alpha ** 0.5) + eps * ((1 - alpha) ** 0.5), eps
+
+    def forward_diffusion_t(self, x0: torch.Tensor, t: int):
+        assert t <= self.n_steps
+
         if t == 0:
-            return model_mean
-        else:
-            noise = torch.randn_like(x)
-            return model_mean + torch.sqrt(posterior_variance_t) * noise
+            return x0
+        
+        eps = torch.randn_like(x0)
+        A   = torch.prod(self.alphas[:t])
+
+        r   = torch.sqrt(A) * x0 + torch.sqrt(1 - A) * eps
+
+        return r
+    
+    def denoise(self, x: torch.Tensor, eps: torch.Tensor, alpha: int = 1):
+        return (x - eps * torch.sqrt(1 - alpha)) / (alpha ** 0.5)
+    
+    def denoise_batch(self, x: torch.Tensor, eps: torch.Tensor, alpha: int):
+        r = []
+
+        for i in range(x.shape[0]):
+            r.append(self.denoise(x[i], eps[i], alpha)[None])
+        
+        r = torch.cat(r, dim=0)
+
+        return r
     
     @torch.no_grad()
     def sample(self, n, input_shape=(3, 64, 64), device="cpu"):
         self.model.eval()
         xt = torch.randn((n, *input_shape), device=device)
+
         for i in range(0, self.n_steps)[::-1]:
             t   = torch.full((n,), i, device=device, dtype=torch.long)
+
             eps = self.model(xt, t)
-            xt  = self.sample_timestep(xt, eps, t)
+            xt  = self.denoise_batch(xt, eps, self.alphas[i])
+
         self.model.train()
         return xt
     
     @torch.no_grad()
     def evalute(self, c=10, input_shape=(3, 64, 64), device="cpu"):
         self.model.eval()
+
         xt = torch.randn((1, *input_shape), device=device)
         stepsize = int(self.n_steps / c)
         xr = []
         
         for i in range(0, self.n_steps)[::-1]:
             t   = torch.full((1,), i, device=device, dtype=torch.long)
+
             eps = self.model(xt, t)
-            xt  = self.sample_timestep(xt, eps, t)
+            xt  = self.denoise_batch(xt, eps, self.alphas[i])
             
             if i % stepsize == 0:
                 xr.append(xt.clone().detach())
+
         self.model.train()
         xr = torch.cat(xr, dim=0)
         
--- models/unet.py
+++ models/unet.py
@@ -3,9 +3,24 @@ import torch.nn.functional as F
 import torch.nn as nn
 import torch
 
-from .position_embeddings import *
+import math
 
 from typing import List
+
+
+class SinusoidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim: int, n_steps: int = 1000):
+        super().__init__()
+        self.dim = dim
+    
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+        return embeddings
 
 
 class BasicBlock(nn.Module):
@@ -14,12 +29,6 @@ class BasicBlock(nn.Module):
 
         self.main = nn.Sequential(
             nn.Conv2d(input_c, out_c, 3, stride=1, padding=1),
-            nn.LeakyReLU(), nn.BatchNorm2d(out_c),
-
-            nn.Conv2d(out_c, out_c, 3, stride=1, padding=1),
-            nn.LeakyReLU(), nn.BatchNorm2d(out_c),
-
-            nn.Conv2d(out_c, out_c, 3, stride=1, padding=1),
             nn.LeakyReLU(), nn.BatchNorm2d(out_c),
 
             nn.Conv2d(out_c, out_c, 3, stride=1, padding=1),
@@ -37,9 +46,6 @@ class UpsampleBlock(nn.Module):
         self.main = nn.Sequential(
             nn.ConvTranspose2d(input_c, out_c, 4, stride=2, padding=1),
             nn.LeakyReLU(), nn.BatchNorm2d(out_c),
-
-            nn.Conv2d(out_c, out_c, 3, stride=1, padding=1),
-            nn.LeakyReLU(), nn.BatchNorm2d(out_c),
         )
     
     def forward(self, x):
@@ -47,100 +53,83 @@ class UpsampleBlock(nn.Module):
     
 
 class Down(nn.Module):
-    def __init__(self, input_c: int, out_c: int, time_emb_c: int = 64, 
+    def __init__(self, input_c: int, out_c: int, time_emb_dim: int = 32,
                  block = BasicBlock, pool = F.max_pool2d) -> None:
         
         super().__init__()
 
-        self.main = block(input_c + time_emb_c, out_c)
+        self.main = block(input_c, out_c)
         self.pool = pool
+
+        self.fc = nn.Sequential(
+            nn.Linear(time_emb_dim, input_c * 2),
+            nn.SiLU(), nn.LayerNorm(input_c * 2),
+
+            nn.Linear(input_c * 2, input_c),
+            nn.SiLU(), nn.LayerNorm(input_c),
+        )
     
-    def forward(self, x, emb):
-        skip = self.main(torch.cat((x, emb), dim=1))
+    def forward(self, x, t):
+        t = self.fc(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+
+        skip = self.main(x + t)
         x    = self.pool(skip, kernel_size=(2, 2))
 
         return x, skip
     
 
 class Up(nn.Module):
-    def __init__(self, input_c: int, skip_c: int, out_c: int, time_emb_c: int = 64, 
+    def __init__(self, input_c: int, skip_c: int, out_c: int, time_emb_dim: int = 32,
                        up_sample = UpsampleBlock, block = BasicBlock) -> None:
         
         super().__init__()
 
         self.up_sample = up_sample(input_c, input_c)
-        self.block     = block(input_c + skip_c + time_emb_c, out_c)
-    
-    def forward(self, x, skip, time_emb):
-        x = self.up_sample(x)
+        self.block     = block(input_c + skip_c, out_c)
 
-        x = torch.cat((x, skip, time_emb), dim=1)
+        self.fc = nn.Sequential(
+            nn.Linear(time_emb_dim, input_c * 2),
+            nn.SiLU(), nn.LayerNorm(input_c * 2),
+
+            nn.Linear(input_c * 2, input_c),
+            nn.SiLU(), nn.LayerNorm(input_c),
+        )
+    
+    def forward(self, x, skip, t):
+        t = self.fc(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        x = self.up_sample(x + t)
+
+        x = torch.cat((x, skip), dim=1)
         x = self.block(x)
 
         return x
-    
-
-class TimeEncoder(nn.Module):
-    def __init__(self, input_c: int = 16, emb_c: int = 64, n_layers: int = 4) -> None:
-        super().__init__()
-
-        self.conv = nn.Sequential(
-            nn.Conv2d(input_c, emb_c, 3, stride=1, padding=1),
-            nn.LeakyReLU(), nn.BatchNorm2d(emb_c)
-        )
-
-        self.main = nn.ModuleList([
-            nn.Sequential(
-                nn.ConvTranspose2d(emb_c, emb_c, 4, stride=2, padding=1),
-                nn.LeakyReLU(), nn.BatchNorm2d(emb_c),
-
-                nn.Conv2d(emb_c, emb_c, 3, stride=1, padding=1),
-                nn.LeakyReLU(), nn.BatchNorm2d(emb_c),
-
-                nn.Conv2d(emb_c, emb_c, 3, stride=1, padding=1),
-                nn.LeakyReLU(), nn.BatchNorm2d(emb_c)
-            )
-
-            for _ in range(n_layers)
-        ])
-    
-    def forward(self, x):
-        x   = self.conv(x)
-        emb = []
-
-        for layer in self.main:
-            x = layer(x)
-            emb.append(x)
-
-        return emb 
 
 
 class Unet(nn.Module):
-    def __init__(self, input_c:    int, 
-                       c:          List[int] = [32, 64, 128, 256], 
-                       ls_c:       int       = 512, 
-                       input_size: int       = 64,
-                       up_sample             = UpsampleBlock, 
-                       block                 = BasicBlock, 
-                       pool                  = F.max_pool2d,
-                       time_emb              = SinusoidalPositionEmbeddings,
-                       time_encoder          = TimeEncoder,
-                       time_encoder_c        = 64,
-                       fn                    = lambda x: x ) -> None:
+    def __init__(self, input_c:  int, 
+                       c:        List[int] = [32, 64, 128, 256], 
+                       ls_c:     int       = 512, 
+                       up_sample           = UpsampleBlock, 
+                       block               = BasicBlock, 
+                       pool                = F.max_pool2d,
+                       time_encoder        = SinusoidalPositionEmbeddings,
+                       time_emb_dim        = 32,
+                       n_steps             = 1000,
+                       fn                  = None ) -> None:
         
         super().__init__()
 
         c.insert(0, input_c)
-
-        assert input_size / (2 ** (len(c) - 1)) == 4
+        
+        self.time_encoder = time_encoder(dim=time_emb_dim, n_steps=n_steps)
 
         self.down_layers = nn.ModuleList([
-            Down(c[i], c[i + 1], block=block, pool=pool)
+            Down(c[i], c[i + 1], block=block, pool=pool, time_emb_dim=time_emb_dim)
             for i in range(len(c) - 1)
         ])
 
         self.up_layers   = nn.ModuleList([
-            Up(c[i + 1], c[i + 1], c[i], up_sample=up_sample, block=block)
+            Up(c[i + 1], c[i + 1], c[i], up_sample=up_sample, block=block, time_emb_dim=time_emb_dim)
             for i in range(len(c) - 1)[::-1]
         ])
 
@@ -156,31 +145,24 @@ class Unet(nn.Module):
             nn.Conv2d(c[0], input_c, 1, stride=1, padding=0),
         )
 
-        self.time_emb = nn.Sequential(
-            time_emb(2 * 4 * 4),
-
-            nn.Linear(2 * 4 * 4, 16 * 4 * 4), nn.SiLU(),
-            nn.Unflatten(1, (16, 4, 4))
-        )
-
-        self.time_encoder = time_encoder(16, time_encoder_c, n_layers=len(c) - 1)
-
         self.fn = fn
     
     def forward(self, x, t):
-        time_emb = self.time_encoder(self.time_emb(t))
+        t = self.time_encoder(t)
         s = []
 
-        for layer, emb in zip(self.down_layers, time_emb[::-1]):
-            x, skip = layer(x, emb)
+        for layer in self.down_layers:
+            x, skip = layer(x, t)
             s.append(skip)
         
         x = self.ls_conv(x)
 
-        for layer, skip, emb in zip(self.up_layers, s[::-1], time_emb):
-            x = layer(x, skip, emb)
+        for layer, skip in zip(self.up_layers, s[::-1]):
+            x = layer(x, skip, t)
         
         x = self.out_c(x)
-        x = self.fn(x)
+        
+        if self.fn is not None:
+            x = self.fn(x)
 
         return x
